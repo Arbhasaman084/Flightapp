5d84696e72711c76f8ca60c830926c5547e21276
Arbhasaman084 <115079498+Arbhasaman084@users.noreply.github.com>
2022-10-05 23:54:01 +0530

    TicketApp

---
+++ TicketApp Project/Ticket BackEnd/src/test/java/com/ohm/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohm.Nakul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
